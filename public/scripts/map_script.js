1313c7dcdee7d7e33559b55a43ea7fc6cf92e16d
KTZHANG <kennyz620@hotmail.com>
2022-10-01 11:09:32 -0700

    Fixed skeleton code

---
+++ public/scripts/map_script.js
@@ -0,0 +1,14 @@
+﻿var minZoomV = 3;
+var maxZoomV = 18;
+var homeCoords = [49.27767013573553, -122.91268085603525];
+var mapSize = document.getElementById("map");
+var lightStyle = 'https://tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=qYOU04zmJXYprHE89esvVcT3qGW68VsSgDdYXjXUUmZgDRBajbH3e58EHY5bONXU';
+
+var map = L.map('map', {
+    minZoom: minZoomV,
+    maxZoom: maxZoomV,
+    zoomSnap: 1
+}).setView(homeCoords, 3);
+
+var tiles = L.tileLayer(lightStyle, {}).addTo(map);
+map.attributionControl.addAttribution("<a href=\"https://www.jawg.io\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors")
