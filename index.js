4a686ab0ef0573885c539f31d5d44489425ef2a9
KTZHANG <kennyz620@hotmail.com>
2022-10-01 18:49:20 -0700

    add db files

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express')
+const url = require('url')
+const axios = require('axios')
+const cors = require("cors") 
 const path = require('path')
+const { pool } = require("./dbConfig");
+
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/map', (req, res) => res.render('pages/map'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app = express();
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.get('/map', (req, res) => res.render('pages/map'))
+
+  app.get('/addr', async (req, res) => {
+    try {
+        var commandstoDB = `SELECT * FROM sap_locations`
+
+        const client = await pool.connect();
+        const result = await client.query(commandstoDB);
+        const data = { results: result.rows };
+
+        res.json(data);
+        status = 0;
+        client.release();
+    }
+    catch (error) {
+        console.log('X->', error);
+        status = -2;
+        res.json("None.")
+    }
+  });
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- test.js
+++ test.js
@@ -1,28 +1,78 @@
-const { spawn } = require('child_process');
-const got = require('got');
-const test = require('tape');
+const express = require("express");
+const app = express();
+const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash =require("express-flash");
+const passport = require("passport");
+const cors = require("cors") //cross-origin resource sharing
+const axios = require('axios')
+const url = require('url')
 
-// Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
+const globalAPIKey = "dQovNOSOTUaNoNZcs6Yvon0WyjM6JeULJSAbL2effgzxknVhAh"; 
 
-test('responds to requests', (t) => {
-  t.plan(4);
+//const bodyParser = require('body-parser');
+const path = require('path')
 
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    (async () => {
-      const response = await got('http://127.0.0.1:5000');
-      // stop the server
-      child.kill();
-      // No error
-      t.false(response.error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(response.body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(response.body.indexOf("Getting Started on Heroku with Node.js"), -1);
-    })();
-  });
+
+const PORT = process.env.PORT || 5010;
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+//app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/map', (req, res) => res.render('pages/map'))
+app.get('/addr', async (req, res) => {
+    try {
+        var commandstoDB = `SELECT * FROM sap_locations`
+
+        const client = await pool.connect();
+        const result = await client.query(commandstoDB);
+        const data = { results: result.rows };
+
+        res.json(data);
+        status = 0;
+        client.release();
+    }
+    catch (error) {
+        console.log('X->', error);
+        status = -2;
+        res.json("None.")
+    }
 });
+
+app.post('/append', async (req,res) => {
+	
+	console.log(req.body);
+
+	if (req.body.id != null) { 
+    	try {
+        var commandstoDB = `UPDATE sap_locations SET xcoordinate = ${req.body.lat}, ycoordinate = ${req.body.long} WHERE id = ${req.body.id};`
+
+        const client = await pool.connect();
+        const result = await client.query(commandstoDB);
+        status = 0;
+        client.release();
+		res.send("Horray!");
+		
+    	}
+    	catch (error) {
+        console.log('X->', error);
+        status = -2;
+        res.json("None.")
+    	}
+	}
+	else {
+	res.send("Yay?");
+}
+	
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app; // allows us to export app for use in testing
