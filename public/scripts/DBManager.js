abb22cc6427c0c6489d4991c048940cba8f7e72f
KTZHANG <kennyz620@hotmail.com>
2022-10-01 18:41:54 -0700

    C

---
+++ public/scripts/DBManager.js
@@ -0,0 +1,24 @@
+﻿function extractDBData(url) {
+    var txtFile = new XMLHttpRequest();
+    var parsedD = null;
+
+    txtFile.open("GET", url, false);
+    txtFile.onload = function (e) {
+        if (txtFile.readyState === 4) {
+            if (txtFile.status === 200) {
+                var csvData = txtFile.responseText;
+
+                parsedD = csvData;
+            } else {
+                console.error(txtFile.statusText);
+            }
+        }
+    };
+    txtFile.onerror = function (e) {
+        console.error(txtFile.statusText);
+    };
+
+    txtFile.send();
+
+    return JSON.parse(parsedD);
+}
